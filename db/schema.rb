e22b6fb3e153903f4c0d3a5a91d335cb3ea63265
Mariano Schmidt <marianogabriels@gmail.com>
2024-05-13 20:36:23 -0300

    Implemented Pg database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_13_222507) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"

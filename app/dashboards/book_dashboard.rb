c50481dcfb33cab7900be8d65cbf41933cfa40e3
Mariano Schmidt <marianogabriels@gmail.com>
2024-05-14 18:10:11 -0300

    Display correctly the book title/author

--- app/dashboards/book_dashboard.rb
+++ app/dashboards/book_dashboard.rb
@@ -69,7 +69,7 @@ class BookDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how books are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(book)
-  #   "Book ##{book.id}"
-  # end
+   def display_resource(book)
+     "#{book.title} - #{book.author}"
+   end
 end

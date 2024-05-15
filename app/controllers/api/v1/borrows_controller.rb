96ca5a9dbb0cc062d3075868c96cef1970010403
Mariano Schmidt <marianogabriels@gmail.com>
2024-05-14 22:52:38 -0300

    Add Api/v1
    - support for borrow management
    - support for book management

---
+++ app/controllers/api/v1/borrows_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::BorrowsController < ApplicationController
+  before_action :set_borrow, only: [:show, :destroy, :mark_as_returned]
+
+  # GET /api/v1/borrows
+  def index
+    borrows = Borrow.all
+    render json: borrows, status: :ok
+  end
+
+  # GET /api/v1/borrows/:id
+  def show
+    render json: @borrow, status: :ok
+  end
+
+  # POST /api/v1/borrows
+  def create
+    borrow = Borrow.new(borrow_params)
+    if borrow.save
+      render json: borrow, status: :created
+    else
+      render json: borrow.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /api/v1/borrows/:id/mark_as_returned
+  def mark_as_returned
+    if @borrow.update(returned_at: Time.current)
+      render json: @borrow, status: :ok
+    else
+      render json: @borrow.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/borrows/:id
+  def destroy
+    @borrow.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_borrow
+    @borrow = Borrow.find(params[:id])
+  end
+
+  def borrow_params
+    params.require(:borrow).permit(:user_id, :book_id, :due_at, :returned_at)
+  end
+end
